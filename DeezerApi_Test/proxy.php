a90f8bac2aa0bd44d31061e5f9a949b3c4dc387b
Patrick HENG <patrick.heng@hetic.net>
2014-10-08 09:47:09 +0200

    Revert "Summary"
    This reverts commit 6307ae24335954f26144830652d4b2552a36d8a1.

--- DeezerApi_Test/proxy.php
+++ DeezerApi_Test/proxy.php
@@ -1,8 +1,8 @@
 <?php
 
 if(isset($_POST['geturl']) and !empty($_POST['geturl'])) {
-	$data = file_get_contents($_POST['geturl']);
-	print $data;
+$data = file_get_contents($_POST['geturl']);
+print $data;
 }
 
 ?>
