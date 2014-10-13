5439d94f9b5134b9b69163ed74bc92ab50b0b83b
Nclsndr <nclsndr@195-154-72-212.online.net>
2014-10-14 00:33:30 +0200

    Traitement des params url GET avec Node

--- app/webroot/ng/server.js
+++ app/webroot/ng/server.js
@@ -1,14 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
+
 var io = require('socket.io')(http);
-
-var url = require('url');
-var qString = require('querystring');
-
-app.get('/', function(req, res){
-	res.sendfile('index.html');
-});
-
 io.on('connection', function(socket){
 	console.log('a user connected');
 	// console.log(socket.request);
@@ -19,14 +12,54 @@ io.on('connection', function(socket){
 });
 
 
+app.get('/', function(req, res){
+	res.sendfile('index.html');
+});
+
+app.use(function (req, res, next) {
+	console.log(req.url);
+	io.on('connection', function(socket){
+		// LOGIQUE CONDITIONNELLE DU TRAITEMENT TOKEN
+	  io.emit('message', req.url);
+	});
+	res.sendfile('index.html');
+  console.log('Time: %d', Date.now());
+});
 
 
-// io.on('connection', function(socket){
-//   socket.on('chat message', function(msg){
-//     io.emit('chat message', msg);
-//   });
-// });
+
 
 http.listen(3000, function(){
 	console.log('listening on *:3000');
 });
+
+
+
+// var url = require('url');
+// var qString = require('querystring');
+// var fs = require('fs');
+
+// fs.readFile('index.html', function (err, data) {
+//     if (err) {
+//         throw err;
+//     }
+//     index = data;
+// });
+
+// var http = require('http');
+// var server = http.createServer(function(req, res){
+// 	res.writeHead('200');
+// 	var page = url.parse(req.url).pathname;
+// 	console.log(page);
+// 	var params = qString.parse(url.parse(req.url).query);
+// 	console.log(params);
+// 	res.write(index);
+// 	res.end();
+// });
+
+// server.listen(3000, function(){
+// 	console.log("server listening on :3000");
+// });
+
+
+
