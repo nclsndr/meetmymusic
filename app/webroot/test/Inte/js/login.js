e6635ed0ac7864f6e2c304ced542d32cdcb15ec6
Patrick HENG <patrick.heng@hetic.net>
2014-10-25 17:13:26 +0200

    Page Integration bug fix

--- app/webroot/test/Inte/js/login.js
+++ app/webroot/test/Inte/js/login.js
@@ -1,9 +1,15 @@
 document.addEventListener('DOMContentLoaded',function(){
 	var windowY = window.innerHeight;
 	var loginPage = document.getElementById('loginPage');
+	var logo = document.querySelector('header img');
+
 	loginPage.style.min-height = (windowY)+"px";
 	window.onresize = function(event) {
 		loginPage.style.min-height = (windowY)+"px";
 	};
+
+	logo.addEventListener('mouseOver', function(){
+		alert('over');
+	})
 
 })
