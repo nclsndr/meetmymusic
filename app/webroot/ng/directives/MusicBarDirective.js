310a1681cd0bce28685716d3071a2cc0cd3faa85
nclsndr <hello@nclsndr.com>
2014-11-22 14:50:47 +0100

    Resolve conflict

--- app/webroot/ng/directives/MusicBarDirective.js
+++ app/webroot/ng/directives/MusicBarDirective.js
@@ -1,8 +1,6 @@
 mmmApp.directive('musicbar', ['SoundcloudService', function (SoundcloudService){
     return {
         restrict: 'E',
-<<<<<<< HEAD
-
         template : '<div class="timeCodeContainer"><span id="tcProgress">{{tcProgressConvert}}</span> / <span id="tcTotal">{{tcTotalConvert}}</span></div><div id="musicBar" class="animated zoomIn"><div id="musicBarCursor"></div></div>',
         
         link: function(scope, element, attrs) {
@@ -12,36 +10,22 @@ mmmApp.directive('musicbar', ['SoundcloudService', function (SoundcloudService){
             // };
             scope.setTC = function(ms) {
                 return SoundcloudService.setTimeCode(ms);
-=======
-        template : '<div class="timeCodeContainer"><span id="tcProgress">{{tcProgressConvert}}</span> / <span id="tcTotal">{{tcTotalConvert}}</span></div><div id="musicBar" class="animated zoomIn"><div id="musicBarCursor"></div></div>',
-        link: function(scope, element, attrs) {
-            scope.setWidth = function(w) {
-                document.getElementById('musicBarCursor').style.width = w;
->>>>>>> init-app-socket
             };
             scope.setTC = function(ms) {
                 return SoundcloudService.setTimeCode(ms);
             };
 
-<<<<<<< HEAD
-=======
-            scope.tcProgressConvert = scope.setTC(scope.tcProgress);
-            scope.tcTotalConvert = scope.setTC(scope.tcTotal); 
->>>>>>> init-app-socket
+
 
             scope.updateBar = function(){
-             if (scope.tcProgress <= scope.tcTotal) {
+                if (scope.tcProgress <= scope.tcTotal) {
                  scope.tcProgressConvert = scope.setTC(scope.tcProgress);
-<<<<<<< HEAD
                  scope.tcTotalConvert = scope.setTC(scope.tcTotal); 
                  scope.cursorPercent = (scope.tcProgress * 100) / scope.tcTotal;
                  // scope.setWidth(scope.cursorPercent+'%');
-=======
-                 scope.cursorPercent = (scope.tcProgress * 100) / scope.tcTotal;
-                 scope.setWidth(scope.cursorPercent+'%');
->>>>>>> init-app-socket
-             }
+                }
             }
+            
             scope.updateBar();
             scope.tcProgressConvert = scope.setTC(scope.tcProgress);
             scope.tcTotalConvert = scope.setTC(scope.tcTotal); 
